778121bdaa924c5d02082253569d2f68aa40173e
reporting-boop <reporting@techno-communications.com>
2026-07-15 17:40:31 -0500

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@
 const CONFIG = {
   // Paste your Google Apps Script Web App URL here.
   // It looks like: https://script.google.com/macros/s/AKfycb.../exec
- DATA_SOURCE_URL: "https://script.google.com/macros/s/AKfycbwJhF8X14DsbwBr4kL61ULcfmWYZC1NVD6LdmhlXHTFCKugRJ5jme-L8favIZSmwPlB/exec",
+ DATA_SOURCE_URL: "https://script.google.com/macros/s/AKfycbwpia2YBWWKwNW3GuUaE6QyOqUvEYphZiKm6KSz7Cc/dev",
   // How often (in minutes) the dashboard silently re-fetches data in
   // the background so open tabs pick up sheet edits automatically.
   // Set to 0 to disable auto-refresh (data still loads fresh on every
